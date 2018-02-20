ff53d8ddbc35039446b2eee9fc5e494a2015b179
André Cardoso <anbc@outlook.com>
2018-02-19 21:10:31 -0300

    Link conf

--- ESP8266/mobile/src/main/java/br/com/hotdogdoedelson/remote/esp8266/LoginActivity.java
+++ ESP8266/mobile/src/main/java/br/com/hotdogdoedelson/remote/esp8266/LoginActivity.java
@@ -1,11 +1,14 @@
 package br.com.hotdogdoedelson.remote.esp8266;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.util.Linkify;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -42,6 +45,29 @@ public class LoginActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        TextView lostPass = (TextView) findViewById(R.id.lostPass);
+        lostPass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent resetPass = new Intent(Intent.ACTION_VIEW,
+                        Uri.parse("https://www.google.com.br/search?q=resetar+senha"));
+                startActivity(resetPass);
+
+            }
+        });
+
+        TextView newAcount = (TextView) findViewById(R.id.newAcount);
+        newAcount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent newAcont = new Intent(Intent.ACTION_VIEW,
+                        Uri.parse("https://www.google.com.br/search?q=nova+conta"));
+                startActivity(newAcont);
+
+            }
+        });
+
 
     }
 }
